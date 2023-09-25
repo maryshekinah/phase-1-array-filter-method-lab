6893a786e324e22383da8823ec6c9ec09c787377
shekinah <nyamburamary1212@gmail.com>
2023-09-25 19:59:41 +0300

    complete array filter method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+// Define the findMatching function
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+// Test cases for the findMatching function
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+
